f0559ac4080a3eae7d4cf253c424516d50d19131
wimuka <avii08el1s@gmail.com>
2021-06-28 16:21:45 -0400

    Fixed images inside must-haves component

--- src/components/MustHaves.js
+++ src/components/MustHaves.js
@@ -1,51 +1,49 @@
 import React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { featuredProducts } from '../config.js';
 import './styles/must-haves.scss';
 import './styles/typography.scss';
 
 const MustHaves = () => {
+  //* query all images from 'src/images' folder
+  //* original name - to find image that matches item from config; gatsbyImageData to be passed to getImage (must)
+  const imgQuery = useStaticQuery(graphql`
+    query MyQuery {
+      allImageSharp {
+        nodes {
+          gatsbyImageData(width: 500)
+          fluid {
+            originalName
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <>
       <section className='must-haves'>
         <h1 className='heading-large'>Must Haves</h1>
         <div className='must-haves__container'>
-          <div className='must-haves__item'>
-            <StaticImage
-              src='../images/agem-orange_toner.png'
-              alt='Toner Image'
-            />
-            <p className='must-haves__text'>Agem Toner</p>
-            <a href='#/products/toner' className='btn btn--light'>
-              Add To Bag - $20
-            </a>
-          </div>
-          <div className='must-haves__item'>
-            <StaticImage src='../images/agem-green_btl.png' alt='Toner Image' />
-            <p className='must-haves__text'>Magic Serum</p>
-            <a href='#/products/toner' className='btn btn--light'>
-              Add To Bag - $35
-            </a>
-          </div>
-          <div className='must-haves__item'>
-            <StaticImage
-              src='../images/agem-face_cream.png'
-              alt='Toner Image'
-            />
-            <p className='must-haves__text'>Moon Night Cream</p>
-            <a href='#/products/toner' className='btn btn--light'>
-              Add To Bag - $30
-            </a>
-          </div>
-          <div className='must-haves__item'>
-            <StaticImage
-              src='../images/agem-pink_mosturizer.png'
-              alt='Toner Image'
-            />
-            <p className='must-haves__text'>Cherry Blossom Mosturizer</p>
-            <a href='#/products/toner' className='btn btn--light'>
-              Add To Bag - $15
-            </a>
-          </div>
+          {featuredProducts.map(({ name, price, file }, index) => (
+            // map through all items from config and create div w/ img, name & price
+            <div className='must-haves__item' key={`${index}`}>
+              {imgQuery.allImageSharp.nodes.map(item => {
+                // map trough all imgs from query and find one that matches item's name from config
+                if (item.fluid.originalName.includes(file)) {
+                  //once match is found - set image with item's gatsbyImageData
+                  const image = getImage(item.gatsbyImageData);
+                  //return image - make sure to reference gatsbyImageData (doesn't work otherwise)
+                  return <GatsbyImage image={image} alt={`${name}'s Photo`} />;
+                } else return null;
+              })}
+              <p className='must-haves__text'>{name}</p>
+              <a href={`#/products/${name}`} className='btn btn--light'>
+                Add To Bag - {`${price}`}
+              </a>
+            </div>
+          ))}
         </div>
         <a href='#/products' className='btn btn--green'>
           All Products
--- src/config.js
+++ src/config.js
@@ -17,4 +17,28 @@ module.exports = {
       url: '/#contacts',
     },
   ],
+
+  featuredProducts: [
+    {
+      name: 'Moon Night Cream',
+      price: '$30',
+      file: 'agem-face_cream.png',
+    },
+    {
+      name: 'Agem Toner',
+      price: '$20',
+      file: 'agem-orange_toner.png',
+    },
+    {
+      name: 'Magic Serum',
+      price: '$35',
+      file: 'agem-green_btl.png',
+    },
+
+    {
+      name: 'Cherry Blossom Mosturizer',
+      price: '$15',
+      file: 'agem-pink_mosturizer.png',
+    },
+  ],
 };
