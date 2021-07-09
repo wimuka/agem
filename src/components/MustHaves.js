3ebdc8723bad8524808ecf9a005ab58190a85e93
wimuka <avii08el1s@gmail.com>
2021-07-09 15:57:41 -0400

    Optimizing images

--- src/components/MustHaves.js
+++ src/components/MustHaves.js
@@ -26,6 +26,7 @@ const MustHaves = () => {
                       image={image}
                       alt={`${name}'s Photo`}
                       key={`${index}+1`}
+                      layout='constrained'
                     />
                   );
                 } else return null;
