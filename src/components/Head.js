19df6d5c95962b750a113d193cbdcefc6ce34540
wimuka <avii08el1s@gmail.com>
2021-07-09 13:12:48 -0400

    Optimization edits

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,17 +6,9 @@ const Head = () => {
     <>
       <Helmet title='AGEM'>
         <html lang='en' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700'
-          rel='stylesheet'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Belleza'
-          rel='stylesheet'
-        />
         <meta charset='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta name='description' content='Natural skincare website'></meta>
+        <meta name='description' content='Natural skincare products'></meta>
       </Helmet>
     </>
   );
